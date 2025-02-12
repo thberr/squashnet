ca264e65d1b5534633bb15f0c059b966b5240b88
Théo Berr <theo.berr@etudiant.univ-lr.fr>
2025-02-12 11:31:16 +0100

    :iphone: fixed footer responsive design

--- components/footer.vue
+++ components/footer.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { CarouselResponsiveOptions } from "primevue";
+
 const partners = ref<{ name: string; img: string }[]>([
   { name: "European Squash", img: "img/partners/logo_esf.png" },
   { name: "Agence Nationale du Sport", img: "img/partners/logo_ans.png" },
@@ -6,36 +8,59 @@ const partners = ref<{ name: string; img: string }[]>([
   { name: "Dunlop", img: "img/partners/logo_dunlop.png" },
   { name: "ASB", img: "img/partners/logo_asb.png" },
 ]);
+
+const carouselResponsiveOptions = ref<CarouselResponsiveOptions[]>([
+  {
+    breakpoint: "1280px",
+    numVisible: 4,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "1024px",
+    numVisible: 3,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "768px",
+    numVisible: 2,
+    numScroll: 1,
+  },
+  {
+    breakpoint: "640px",
+    numVisible: 1,
+    numScroll: 1,
+  },
+]);
 </script>
 
 <template>
-  <div class="flex flex-col h-full justify-end">
-    <div class="mb-6">
-      <Carousel
-        :value="partners"
-        :numVisible="3"
-        :numScroll="1"
-        :showIndicators="false"
-        :autoplayInterval="3000"
-        circular
-      >
-        <template #header>
-          <div class="flex justify-center text-2xl mb-8">LES PARTENAIRES</div>
-        </template>
+  <div class="flex flex-col h-full justify-end gap-8">
+    <Carousel
+      :value="partners"
+      :numVisible="4"
+      :showIndicators="false"
+      :autoplayInterval="3000"
+      :responsiveOptions="carouselResponsiveOptions"
+      circular
+    >
+      <template #header>
+        <div class="flex justify-center text-2xl mb-8">LES PARTENAIRES</div>
+      </template>
 
-        <template #item="partner">
-          <div class="flex h-full justify-center items-center">
-            <NuxtPicture
-              :src="partner.data.img"
-              :alt="partner.data.name"
-              sizes="120px"
-            />
-          </div>
-        </template>
-      </Carousel>
-    </div>
+      <template #item="partner">
+        <div class="flex h-full justify-center items-center">
+          <NuxtPicture
+            :src="partner.data.img"
+            :alt="partner.data.name"
+            sizes="120px"
+          />
+        </div>
+      </template>
+    </Carousel>
 
-    <div class="flex flex-row gap-8 justify-center items-center h-[7%]">
+    <div
+      class="flex flex-col gap-8 justify-center items-center mb-4 md:flex-row md:h-[7%]"
+    >
       <a
         href="https://www.ffsquash.com/fr/squashnet-mentions-legales.html"
         class="no-underline"
